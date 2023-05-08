3d83fece7ecebf43cc71c090cc1ffd6923e30274
nandanvsh <94284968+nandanvsh@users.noreply.github.com>
2023-05-08 15:02:14 +0700

    Update tampilan jadwal

--- resources/views/pages/jadwal/index.blade.php
+++ resources/views/pages/jadwal/index.blade.php
@@ -2,5 +2,7 @@
 @section("content")
     @include("components.navbar")
     <main class="bg-bg2 h-[calc(100vh-4rem)] flex justify-center items-center">
-
+        <div>
+            
+        </div>
     </main>
